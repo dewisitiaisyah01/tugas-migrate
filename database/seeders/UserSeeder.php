de42b7e7db744717afaf028c3ca1e1ac58be9335
dewisitiaisyah01 <dsiti9612@gmail.com>
2024-05-15 15:52:34 +0700

    done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use illuminate\Support\Facades\Hash;
+use illuminate\Support\Str;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'dewi',
+            'email' => '@exampledew.com',
+            'password' => 'dew',
+        ]);  
+    }
+}
